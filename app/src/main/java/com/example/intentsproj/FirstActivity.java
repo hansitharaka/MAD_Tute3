db9cb4e86a2806e8c380819e147dcb4080b15543
HansaniTharaka <66997469+HansaniTharaka@users.noreply.github.com>
2020-08-13 17:38:25 +0530

    onClick event of OK button, Toast message

--- app/src/main/java/com/example/intentsproj/FirstActivity.java
+++ app/src/main/java/com/example/intentsproj/FirstActivity.java
@@ -2,22 +2,63 @@ package com.example.intentsproj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class FirstActivity extends AppCompatActivity {
+
+    public static final String INPUT_1 = "com.example.intentsproj.INPUT1";
+    public static final String INPUT_2 = "com.example.intentsproj.INPUT2";
+    EditText editText1, editText2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+        editText1 = (EditText)findViewById(R.id.num1_in);
+        editText2 = (EditText)findViewById(R.id.num2_in);
+
+        final Button btnOk = findViewById(R.id.btn_Ok);
+
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String input1 = editText1.getText().toString();
+                String input2 = editText2.getText().toString();
+
+                //send multiple data using a bundle
+                /*Bundle bundle = new Bundle();
+                bundle.putString("in1", input1);
+                bundle.putString("in2", input2);*/
+
+                Intent myIntent = new Intent(FirstActivity.this, SecondActivity.class);
+                //myIntent.putExtras(bundle);    //use this if you do it with a bundle
+                myIntent.putExtra(INPUT_1, input1);
+                myIntent.putExtra(INPUT_2, input2);
+                startActivity(myIntent);
+
+                //Toast
+                Context context = getApplicationContext();
+                CharSequence message = "Sending message...";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, message, duration);
+                toast.show();
+
+            }
+        });
+
     }
 
-    public void sendOkMessage(View view) {
-        Intent myIntent = new Intent(this, SecondActivity.class);
-        startActivity(myIntent);
-    }
+   /* public void sendOkMessage(View view) {
+
+    }*/
 
 }
